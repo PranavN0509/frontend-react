fd7db70aca8f8357825f550266242a1294947206
PranavN0509 <21102030.pranav.nagothanekar@gmail.com>
2025-08-10 23:20:26 +0530

    updated api.js

--- src/api.js
+++ src/api.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
-const isDevelopment = import.meta.env.MODE === 'development'
-const mybaseURL = isDevelopment ? import.meta.env.VITE_API_BASE_URL_LOCAL : import.meta.env.VITE_API_BASE_URL_DEPLOYMENT
+const isDevelopment = import.meta.env.MODE === 'development';
+const mybaseURL = isDevelopment ? import.meta.env.VITE_API_BASE_URL_LOCAL : import.meta.env.VITE_API_BASE_URL_DEPLOYMENT;
+
 
 export const api = axios.create({
-  baseURL: `${mybaseURL}/api`,
+  baseURL: "https://backend-django-6qcx.onrender.com/api",
 });
